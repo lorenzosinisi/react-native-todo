4511f081f48edc6a50a28bae3fdc9121cde2d3c8
Lorenzo Sinisi <lasslo.net@gmail.com>
2016-09-11 16:06:34 +0200

    Initial commit

---
+++ index.ios.js
@@ -0,0 +1,109 @@
+import React, { Component } from 'react';
+import * as firebase from 'firebase';
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
+const firebaseConfig = require('./firebase.config.js');
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+import {
+  AppRegistry,
+  TouchableHighlight,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  ListView,
+  AlertIOS
+} from 'react-native';
+
+class todoFirebase extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({ rowHasChanged: (row1, row2) => row1 !== row2 })
+    };
+    this.itemsRef = this.getRef().child('items');
+  }
+
+  getRef() {
+    return firebaseApp.database().ref();
+  }
+
+  componentDidMount() {
+    this.listenForItems(this.itemsRef);
+  }
+
+  listenForItems(itemsRef) {
+    itemsRef.on('value', (snap) => {
+
+      // get children as an array
+      var items = [];
+      snap.forEach((child) => {
+        items.push({
+          title: child.val().title,
+          _key: child.key
+        });
+      });
+
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(items)
+      });
+
+    });
+  }
+  _addItem() {
+    AlertIOS.prompt(
+      'Add New Item',
+      null,
+      [
+        {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+        {
+          text: 'Add',
+          onPress: (text) => {
+            this.itemsRef.push({ title: text })
+          }
+        },
+      ],
+      'plain-text'
+    );
+  }
+  _renderItem(item) {
+
+    const onPress = () => {
+      AlertIOS.alert(
+        'Complete',
+        null,
+        [
+          {text: 'Complete', onPress: (text) => this.itemsRef.child(item._key).remove()},
+          {text: 'Cancel', onPress: (text) => console.log('Cancelled')}
+        ]
+      );
+    };
+
+    return (
+      <ListItem item={item} onPress={onPress} />
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <StatusBar title="Best places in Berlin" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          style={styles.listview}/>
+
+        <ActionButton onPress={this._addItem.bind(this)} title="Add" />
+
+      </View>
+    );
+  }
+}
+AppRegistry.registerComponent('todoFirebase', () => todoFirebase);
